9d6c5cd3254c59b93bc54585f0531d61fea9f330
Wissa <wissa.azmy@gmail.com>
2019-07-29 14:43:44 +0200

    Create Data Model

---
+++ AltEdu/Student+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Student+CoreDataClass.swift
+//  AltEdu
+//
+//  Created by Wissa Azmy on 7/29/19.
+//  Copyright © 2019 Wissa Michael. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Student)
+public class Student: NSManagedObject {
+
+}
