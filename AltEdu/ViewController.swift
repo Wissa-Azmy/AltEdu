39c45a9bf1a3ff4c5cffb0ada575ca7cfc1acb87
Wissa <wissa.azmy@gmail.com>
2019-07-29 14:07:23 +0200

    Initial Commit

---
+++ AltEdu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AltEdu
+//
+//  Created by Wissa Azmy on 7/29/19.
+//  Copyright © 2019 Wissa Michael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
