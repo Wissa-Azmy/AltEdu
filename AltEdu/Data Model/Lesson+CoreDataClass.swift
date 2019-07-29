0caa4495775d26e84fb33a8198687f653aa7f19f
Wissa <wissa.azmy@gmail.com>
2019-07-29 14:40:57 +0200

    Create Data Model

---
+++ AltEdu/Data Model/Lesson+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Lesson+CoreDataClass.swift
+//  AltEdu
+//
+//  Created by Wissa Azmy on 7/29/19.
+//  Copyright © 2019 Wissa Michael. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Lesson)
+public class Lesson: NSManagedObject {
+
+}
---
+++ AltEdu/Data Model/Student+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  Student+CoreDataProperties.swift
+//  AltEdu
+//
+//  Created by Wissa Azmy on 7/29/19.
+//  Copyright © 2019 Wissa Michael. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Student {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Student> {
+        return NSFetchRequest<Student>(entityName: "Student")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var lesson: Lesson?
+
+}
